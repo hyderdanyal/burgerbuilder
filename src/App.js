1c7dedd725df517f5761aae804a51bbc7b79cf2e
hyderdanyal <hyderdanyal@gmail.com>
2021-01-13 02:43:30 -0600

    lazy loading and cleaner code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
 import Layout from "../src/components/Layout/layout"
 import BurgerBuilder from "../src/containers/BurgerBuilder/burgerbuilder"
-import Checkout from "../src/containers/Checkout/Checkout"
-import Orders from "../src/containers/Orders/Orders"
-import Auth from "../src/containers/Auth/Auth"
+// import Checkout from "../src/containers/Checkout/Checkout"
+// import Orders from "../src/containers/Orders/Orders"
+// import Auth from "../src/containers/Auth/Auth"
 import {Route,withRouter, Redirect} from "react-router-dom"
 import Logout from "./containers/Auth/Logout/Logout";
 import {connect} from 'react-redux'
 import * as actions from "./store/actions/index"
+import asyncComponent from "./higherordercomponent/asyncComponent/asyncComponent"
+
+const asyncCheckout = asyncComponent(() =>{
+  return import("../src/containers/Checkout/Checkout")
+})
+const asyncOrders = asyncComponent(() =>{
+  return import("../src/containers/Orders/Orders")
+})
+const asyncAuth = asyncComponent(() =>{
+  return import("../src/containers/Auth/Auth")
+})
 
 class App extends Component {
   // state= {
@@ -26,7 +37,7 @@ class App extends Component {
   render(){
     let routes=(
       <Layout>
-      <Route path="/auth" exact component={Auth}/>
+      <Route path="/auth" exact component={asyncAuth}/>
       <Route path="/" exact component={BurgerBuilder} />
       <Redirect to = "/" />
       </Layout>
@@ -34,9 +45,10 @@ class App extends Component {
     if(this.props.isAuthenticated){
       routes=(
         <Layout>
-        <Route path="/checkout" component={Checkout} />
+        <Route path="/checkout" component={asyncCheckout} />
         <Route path="/" exact component={BurgerBuilder} />
-        <Route path="/orders" exact component={Orders} />
+        <Route path="/auth" exact component={asyncAuth}/>
+        <Route path="/orders" exact component={asyncOrders} />
         <Route path="/logout" exact component={Logout} />
         <Redirect to = "/" />
       </Layout>  
